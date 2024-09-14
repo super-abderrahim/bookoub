56dc18a811ca9ec90b179b90a12d58097fcf0a02
super-abderrahim <148150340+super-abderrahim@users.noreply.github.com>
2024-09-14 15:44:41 +0100

    Fix missing dependency for production build

--- server.js
+++ server.js
@@ -25,14 +25,17 @@ app.use(xss());
 app.use(bodyParser.json({ limit: '10mb' })); // Increase limit as needed // Declare cors only once
 
 
-const corsOptions = {
-  origin: 'https://bookoub.onrender.com',  // Allow only your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Methods you want to allow
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Headers you want to allow
-  credentials: true,  // If you're using cookies or sessions
-};
+const allowedOrigins = ['https://bookoub.onrender.com', 'https://dashboard-krez.onrender.com'];
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 // Create a transporter object with Gmail service
 let transporter = nodemailer.createTransport({
   service: 'gmail',
